6adccfc8e2b078aa123f49fb623536e964185476
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2024-02-14 11:14:36 +0100

    Funkční menu

--- Kalkulacka_EP1A-B/Program.vb
+++ Kalkulacka_EP1A-B/Program.vb
@@ -1,7 +1,27 @@
-Imports System
+﻿Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.BackgroundColor = ConsoleColor.White
+        Console.ForegroundColor = ConsoleColor.Black
+        Console.Clear()
+        Console.CursorVisible = False
+
+        Menu()
+        Console.ReadKey()
+    End Sub
+
+    Sub Menu()
+        'Console.SetCursorPosition(10, 8)
+        Console.WriteLine("╔══════════════════╗")
+        Console.WriteLine("║    KALKULAČKA    ║")
+        Console.WriteLine("╠══════════════════╣")
+        Console.WriteLine("║ 1 - SOUČET       ║")
+        Console.WriteLine("║ 2 - ROZDÍL       ║")
+        Console.WriteLine("║ 3 - SOUČIN       ║")
+        Console.WriteLine("║ 4 - PODÍL        ║")
+        Console.WriteLine("╠══════════════════╣")
+        Console.WriteLine("║ Esc - KONEC      ║")
+        Console.WriteLine("╚══════════════════╝")
     End Sub
 End Module
